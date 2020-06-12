af9dd6e8fe6f37cdd4322b62abcae025374cf124
folarin oyenuga <35351722+olu-folarin@users.noreply.github.com>
2020-06-12 10:09:57 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// for a smooth scrolling effect
+$('.btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800
+    );
+  }
+});
